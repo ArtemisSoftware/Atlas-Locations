7729cc819fd45a789322f14eb9c908d6ca5a113e
gustavo <gustavo.isaac@gmail.com>
2022-03-27 12:28:36 +0100

    AtlasLocationsApplication

---
+++ app/src/main/java/com/artemissoftware/atlaslocations/AtlasLocationsApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.atlaslocations
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AtlasLocationsApplication: Application()
